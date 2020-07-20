f3f8a5a67aefb1d64e44ffab7d0f8e61e8c3d3ad
Amanshu Kataria <amanshukataria@hotmail.com>
2020-07-21 00:20:28 +0530

    increased rate limiter

--- middlerware/rateLimiter.js
+++ middlerware/rateLimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 module.exports = rateLimiterUsingThirdParty = rateLimit({
   windowMs: 1 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  max: 5,
+  max: 20,
   message: {
     message: 'Too many requests, please try again later.'
   },
